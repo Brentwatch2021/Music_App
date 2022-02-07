e3765009d8d478395aa2251e29be8062b435c109
Brentwatch2021 <57631700+Brentwatch2021@users.noreply.github.com>
2022-02-07 14:14:44 +0200

    Adding the Music API Back End

---
+++ Front End/music_app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (<header className='Main-Header'>
+        <img src='' alt='logo'/> 
+        <div className='search-content'></div>
+
+  </header>);
+}
+
+export default Header;
+
